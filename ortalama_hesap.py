038ecfecca888b18323a080aedef0eeb538b1988
surekalkan <151936975+surekalkan@users.noreply.github.com>
2023-11-24 16:50:37 +0300

    ortalama hesap dosyası eklendi

---
+++ ortalama_hesap.py
@@ -0,0 +1,5 @@
+
+matematik = input("matematik  notunuzu giriniz: ")
+ingilizce = input("ingilizce notunuzu giriniz: ")
+ortalama = (float(matematik) + float(ingilizce)) / 2
+print(f"not ortalması {ortalama}'dir. ")
